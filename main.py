6c651a1fec3949d0950ea01873f1dcd107599ca8
Sibin Thomas <54390036+SibinThomasQuad@users.noreply.github.com>
2023-03-26 23:21:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
+#!/usr/bin/env python3
 import os
 import threading
 import json
 import time
+import logging
 
 CRON_PATH = '/var/cron.json'
+LOG_PATH  = '/var/log/cron.log'
+
+logging.basicConfig(filename=LOG_PATH,
+                    format='%(asctime)s %(message)s',
+                    filemode='w')
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
 class File:
-    
+
     def get_crons(self):
         with open(CRON_PATH) as file_in:
             lines = []
@@ -14,38 +24,40 @@ class File:
                     line = line.rstrip('\n')
                     lines.append(line)
             return lines
-    
+
     def create(self):
-    
+
         f = open(CRON_PATH, "a")
         f.write("#------------  CRON TAB -----------------")
         f.close()
 
 class Console:
-	
+
 	def run_command(self,command):
 		os.system(command)
+        
 
 class Cron:
-    
+
     def __init__(self):
         self.console = Console()
         self.file = File()
-    
+
     def cron_start(self,cron):
         cron_data = json.loads(cron)
         time_interval = cron_data['time']
         commad = cron_data['command']
         time.sleep(int(time_interval))
         self.console.run_command(commad)
+        logger.debug(str(commad) +" Command Executed")
         time.sleep(int(time_interval))
         self.cron_start(cron)
-        
+
     def schedule(self,crons):
         for cron in crons:
             crone_thread = threading.Thread(target=self.cron_start, args=(cron,))
             crone_thread.start()
-    
+
     def set_cron_tab(self):
         path = CRON_PATH
         check_file = os.path.isfile(path)
@@ -53,7 +65,7 @@ class Cron:
             self.file.create()
         else:
             self.schedule(self.file.get_crons())
-    
+
     def get_cron(self):
         self.console.run_command("ls")
         print("Working")
